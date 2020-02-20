4eeda3034d1e0c10fbb91d6648d488a90f7a681b
Jan Valkenburg <jan.valkenburg@gmail.com>
2020-02-20 09:44:41 +0100

    Initial commit;

---
+++ README.md
@@ -0,0 +1,32 @@
+# Change Wallpaper MacOS 
+
+This PHP script allows you to change your wallpaper on the Mac. Wallpapers are downloaded from wallhaven.cc.
+
+## Getting Started
+
+To run this script you need to have PHP7 installed on your local machine. 
+
+### Requirements
+
+"/usr/local/bin/wallpaper" needs to be installed on the machine
+PHP 7+
+
+### Installing
+
+Download the script to a local PHP installment and run it within the browser.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Authors
+
+* **Jan Valkenburg** - *Initial work* - [Github](https://github.com/JanValkenburg/mac-wallhaven-change-wallpaper)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+
---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+class App
+{
+    protected $api_key = '5VT3N7UoLwXIADhMrQ31sv2G7DXJXR7N';
+    protected $sorting = 'random';
+    protected $resolutions = '1920x1080';
+    protected $tmpFile = '/Users/vaartland/wallpaper.png';
+    protected $screens = 'all';
+    protected $query = '';
+
+    public function run()
+    {
+        $this->query = $_GET['q'] ?? 'anime girl';
+//        $this->screens = 1;
+        $this->handleImage();
+
+//        $this->screens = 0;
+//        $this->resolutions = '1080x1920';
+//        $this->handleImage();
+    }
+
+    protected function handleImage()
+    {
+        $query = http_build_query([
+            'apikey' => $this->api_key,
+            'resolutions' => $this->resolutions,
+            'q' => 'type:{png}|' . $this->query,
+            'sorting' => $this->sorting
+        ]);
+
+        $url = 'https://wallhaven.cc/api/v1/search?' . $query;
+        $content = file_get_contents($url);
+        $data = json_decode($content);
+
+        if (false === isset($data->data[0]->path)) {
+            echo '<br>';
+            echo 'no image found: '. $this->query . ' ' . $this->resolutions;
+            return null;
+        }
+
+        file_put_contents($this->tmpFile, file_get_contents($data->data[0]->path));
+        shell_exec('/usr/local/bin/wallpaper set --screen ' . $this->screens . ' ' . $this->tmpFile);
+    }
+}
+
+(new App())->run();
+?>
+<meta http-equiv="refresh" content="900">
