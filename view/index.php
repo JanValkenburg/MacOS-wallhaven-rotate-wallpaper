1d321864cfdcbd5c1c27aa330f354f6e39cbb4cc
Jan Valkenburg <jan.valkenburg@gmail.com>
2020-02-26 09:15:35 +0100

    #10 move javascript to external file;

--- view/index.php
+++ view/index.php
@@ -165,39 +165,6 @@
     |
     <a href="https://janvalkenburg.nl/" target="_blank">janvalkenburg.nl</a>
 </footer>
-<script>
-    if (document.querySelector('[name="resolution"]').value === '') {
-        setFormState();
-        if (document.querySelector('[name="resolution"]').value === '') {
-            document.querySelector('[name="resolution"]').value = window.screen.width + 'x' + window.screen.height;
-        }
-    }
-
-    function setFormState() {
-        document.querySelectorAll('input, select').forEach(function (item) {
-            let name = item.getAttribute("name");
-            if (item.getAttribute('type') === 'checkbox') {
-                item.checked = localStorage.getItem(name) === 'on';
-            } else {
-                item.value = localStorage.getItem(name)
-            }
-        });
-    }
-
-    function saveFormState() {
-        document.querySelectorAll('input, select').forEach(function (item) {
-            let name = item.getAttribute("name");
-            let value = item.value;
-            if (item.getAttribute('type') === 'checkbox') {
-                value = 'off';
-                if (document.querySelectorAll('[name = "' + name + '"]:checked').length) {
-                    value = 'on';
-                }
-            }
-            localStorage.setItem(name, value);
-        });
-        return true;
-    }
-</script>
+<script src="view/script.js"></script>
 </body>
 </html>
